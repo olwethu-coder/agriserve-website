0a94b2e15db55781b595fcf350752c3ba0214fc4
olwethu-coder <your-ozondi03@gmail.com>
2026-03-18 11:51:57 +0200

    Increased navbar logo size

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+// Navbar scroll effect
+window.addEventListener('scroll', function() {
+    const navbar = document.getElementById('mainNavbar');
+    if (window.scrollY > 50) { // If page is scrolled more than 50px
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
+
 // Contact form handling
 function handleContactSubmit(event) {
     event.preventDefault();
